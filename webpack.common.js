a1cca22b10e5a1fec9feecd497a318e895d02138
June <adasilva.html@gmail.com>
2023-02-15 16:42:08 -0500

    split webpack config into dev/prod

--- webpack.common.js
+++ webpack.common.js
@@ -1,20 +1,13 @@
-const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
-  devtool: "inline-source-map",
   plugins: [
     new HtmlWebpackPlugin({
       title: "Weather",
       template: "./src/template.html",
     }),
   ],
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-  },
   module: {
     rules: [
       {
