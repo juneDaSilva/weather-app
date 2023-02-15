acc01fc9b83d3caee2f09e8e662bd84762778db8
June <adasilva.html@gmail.com>
2023-02-15 18:10:12 -0500

    Create fetch(weather) function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import "./main.css";
 
-console.log("hello, webpack");
+async function getWeather() {
+  try {
+    let response = await fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=Worcester&APPID=b4b68481d804f19629d4a998e4bd6983",
+      { mode: "cors" }
+    );
+    let weather = await response.json();
+    console.log(weather);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+getWeather();
