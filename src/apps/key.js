cdc6c63fec2071e25e6539458bf977a2fc1b5220
June <adasilva.html@gmail.com>
2023-02-15 20:28:52 -0500

    Create seperate  api key module

---
+++ src/apps/key.js
@@ -0,0 +1,1 @@
+export const key = "b4b68481d804f19629d4a998e4bd6983";
--- src/index.js
+++ src/index.js
@@ -1,16 +1,2 @@
 import "./main.css";
-
-async function getWeather() {
-  try {
-    let response = await fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=Worcester&APPID=b4b68481d804f19629d4a998e4bd6983",
-      { mode: "cors" }
-    );
-    let weather = await response.json();
-    console.log(weather);
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-getWeather();
+import { getWeather } from "./apps/search";
