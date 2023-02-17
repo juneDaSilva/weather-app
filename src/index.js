16737581ac66a0369599ffd3b9075644c54156c0
June <adasilva.html@gmail.com>
2023-02-17 17:22:35 -0500

    Style search container

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "./main.css";
 import { getWeather } from "./apps/fetch";
 import { key } from "./apps/key";
 const searchBtn = document.querySelector(".searchBtn");
-const input = document.getElementById("location");
+const input = document.getElementById("input");
 
 searchBtn.onclick = () => {
   getWeather(input.value, key);
